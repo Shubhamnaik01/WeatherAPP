6ecf8157028ee59286cf1d925c1d0719f23cfa8a
Shubhamnaik01 <shubhamnaik4488@gmail.com>
2025-02-03 20:37:04 +0530

    Basic Fetch weather by lat,long and cityname Implemented

--- App.js
+++ App.js
@@ -1,5 +1,6 @@
 import React from "react";
 import SunriseImage from "./Assets/ForestSunrise.jpg";
+import Suncloud from "./Assets/SunandCloud.svg";
 import "./index.css";
 import Card from "./Card";
 import { useState, useEffect } from "react";
@@ -7,9 +8,11 @@ import { useState, useEffect } from "react";
 const App = () => {
   const [city, setcity] = useState("Mumbai");
   const [weatherData, setWeatherData] = useState(null);
+  const [location, setLocation] = useState(null);
   // console.log(city);
+
   const fetchWeather = async () => {
-    const url = `https://weatherbit-v1-mashape.p.rapidapi.com/current?city=${city}&units=metric&lang=en`;
+    const url = `https://weatherbit-v1-mashape.p.rapidapi.com/current?lon=${location.long}&lat=${location.lat}&units=metrics&lang=en`;
 
     const options = {
       method: "GET",
@@ -29,22 +32,74 @@ const App = () => {
     }
   };
 
+  const fetchWeatherByCity = async () => {
+    const url = `https://weatherbit-v1-mashape.p.rapidapi.com/current?city=${city}&units=metrics&lang=en`;
+
+    const options = {
+      method: "GET",
+      headers: {
+        "x-rapidapi-key": "c65cff5393msh3883091d2818b49p14b2adjsnc602f04e41ee",
+        "x-rapidapi-host": "weatherbit-v1-mashape.p.rapidapi.com",
+      },
+    };
+
+    try {
+      const response = await fetch(url, options);
+      const result = await response.json();
+      setWeatherData(result);
+      console.log(result);
+    } catch (error) {
+      console.error(error);
+    }
+  };
+  function locationSuccess(position) {
+    const lat = position.coords.latitude;
+    const long = position.coords.longitude;
+    console.log(lat + " " + long);
+    setLocation({ lat, long });
+  }
+  function locationFail(err) {
+    console.error("Something went wrong while accessing the location");
+  }
+
+  useEffect(() => {
+    if ("geolocation" in navigator) {
+      navigator.geolocation.getCurrentPosition(locationSuccess, locationFail);
+    }
+  }, []);
+
+  useEffect(() => {
+    if (location) {
+      fetchWeather();
+      console.log(1);
+    }
+  }, [location]);
+
   // fetchWeather();
   return (
     <div className="parent">
       <div className="mainbox">
         <div className="contentBox">
           {weatherData && <h1>{weatherData.data[0].city_name}</h1>}
-          {weatherData && <p>{weatherData.data[0].temp} C</p>}
+          {weatherData && <p>{weatherData.data[0].temp}°C</p>}
+          {/* {true && <h1>{"Mumbai"}</h1>}
+          {true && <p>{36}°C</p>} */}
+          <img src={Suncloud} alt="weatherICON" width={"40%"} height={"40%"} />
           <div className="cardParent">
-            <Card
-              weatherType="Humidity"
-              weatherValue={weatherData.data[0].rh}
-            />
-            <Card
-              weatherType="WindSpeed"
-              weatherValue={weatherData.data[0].wind_spd}
-            />
+            {weatherData && (
+              <Card
+                weatherType="Humidity"
+                weatherValue={weatherData.data[0].rh}
+                units="%"
+              />
+            )}
+            {weatherData && (
+              <Card
+                weatherType="WindSpeed"
+                weatherValue={weatherData.data[0].wind_spd}
+                units="Km/Hr"
+              />
+            )}
           </div>
         </div>
         <div className="searchBox">
@@ -53,16 +108,18 @@ const App = () => {
             onChange={(e) => setcity(e.target.value)}
           ></input>
           <button
-            onClick={fetchWeather}
+            onClick={fetchWeatherByCity}
             style={{
               padding: "10px 20px",
-              backgroundColor: "#3498db",
-              color: "#fff",
+              backgroundColor: "white",
+              color: "Black",
               border: "none",
               borderRadius: "5px",
               cursor: "pointer",
             }}
-          ></button>
+          >
+            Search
+          </button>
         </div>
       </div>
     </div>
--- Card.js
+++ Card.js
@@ -3,7 +3,7 @@ import React from "react";
 function Card(props) {
   return (
     <div className="card">
-      <p>{props.weatherValue}</p>
+      <p>{props.weatherValue + " " + props.units}</p>
       <h2>{props.weatherType}</h2>
     </div>
   );
